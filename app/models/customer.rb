73cf1673e7d165d5078ff3483be22929fd8b21a7
github username <github email address>
2018-07-27 10:36:23 -0400

    challange

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -1,12 +1,47 @@
 class Customer
   attr_accessor :first_name, :last_name
 
+  @@all = []
+
   def initialize(first_name, last_name)
     @first_name = first_name
     @last_name  = last_name
+    @@all << self
   end
 
-  def full_name
-    "#{first_name} #{last_name}"
+
+
+  def add_review (restaurant, rating, content)
+    Review.new(self, restaurant, rating, content)
   end
+
+  def num_reviews
+  number_reviews =  Review.all.select do |reviews|
+      reviews.customer == self
+    end
+    number_reviews.length
+  end
+
+  def self.full_names ##also works as all_names
+    self.all.map {|customer| customer.first_name + " " + customer.last_name}
+  end
+
+
+
+  def self.find_name(full_name)
+
+     splitted = full_name.split(' ')
+     @@all.find do |customer|
+       customer.first_name == splitted[0] && customer.last_name == splitted[1]
+
+     end
+   end
+
+
+  def self.all
+    @@all
+  end
+
+
+
 end
--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -1,8 +1,23 @@
 class Restaurant
   attr_accessor :name
 
+@@all = []
+
   def initialize(name)
     @name = name
+    @@all << self
   end
+
+def self.find_by_name(name)
+      @@all.find do |restaurant|
+        restaurant.name = name
+      end
+
+end
+
+def self.all
+ @@all
+end
+
 
 end
