b2b633101fb30411ec2f94697be709395c8a56ef
Nkosi Enciso Givhan <codeatblack@gmail.com>
2018-07-27 10:36:58 -0400

    Nkosi Enciso Givhan: Finished first mod challenge 1

--- tools/console.rb
+++ tools/console.rb
@@ -7,5 +7,12 @@ end
 # This is a convenient place to define variables and/or set up new object instances,
 # so they will be available to test and play around with in your console
 
+tim = Customer.new("tim", "bob")
+jim = Customer.new("jim", "bob")
+lim = Customer.new("lim", "bob")
+dim = Customer.new("tim", "YOOOOOOb")
+tim = Customer.new("tim", "dsads")
+tim = Customer.new("tim", "shaob")
+
 binding.pry
 0 #leave this here to ensure binding.pry isn't the last line
