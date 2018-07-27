28b8c8d3f6e97bdec6722985d3765e91ff36bf16
Gabriel-Batista <gabedbatista@gmail.com>
2018-07-27 10:30:50 -0400

    Done

--- app/models/review.rb
+++ app/models/review.rb
@@ -1,4 +1,18 @@
 class Review
-  
+    attr_reader :customer, :restaurant
+    attr_accessor :rating, :content
+    @@all = []
+
+    def initialize(customer, restaurant, rating, content)
+        @rating = rating
+        @content = content
+        @restaurant = restaurant
+        @customer = customer
+        @@all << self
+    end
+
+    def self.all
+        @@all
+    end
 end
 
