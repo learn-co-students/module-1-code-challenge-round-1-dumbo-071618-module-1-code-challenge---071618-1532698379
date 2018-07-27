96daec6f2861bc60a7bb218480a7e8ddc8888f8a
github username <github email address>
2018-07-27 10:28:12 -0400

    Done

--- app/models/review.rb
+++ app/models/review.rb
@@ -1,4 +1,25 @@
 class Review
+
+    attr_accessor :customer, :restaurant, :rating, :content
+    @@all = []
+
+    def initialize(customer, restaurant, rating, content)
+        @customer = customer
+        @restaurant = restaurant
+        if rating >5
+            @rating = 5
+        elsif rating < 1
+            @rating = 1
+        else
+            @rating = rating.to_i
+        end
+        @content = content
+        @@all << self
+    end
+
+    def self.all
+        @@all
+    end
   
 end
 
--- tools/console.rb
+++ tools/console.rb
@@ -6,6 +6,24 @@ end
 # Insert code here to run before hitting the binding.pry
 # This is a convenient place to define variables and/or set up new object instances,
 # so they will be available to test and play around with in your console
+amirata = Customer.new("Amirata", "Khodaparast")
+kevin = Customer.new("Kevin", "Rivera")
+bryan = Customer.new("Bryan", "Teng")
+prince = Customer.new("Prince", "Wilson")
+rosa = Customer.new("rosa", "Tu")
+amirata2 = Customer.new("Amirata", "Blah")
+
+ravagh = Restaurant.new("Ravagh")
+persian = Restaurant.new("Persian")
+greek = Restaurant.new("Greek")
+spanish = Restaurant.new("Spanish")
+thai = Restaurant.new("Thai")
+
+amirata.add_review(ravagh, "delicious", 3)
+amirata.add_review(persian, "delicious", 5)
+rosa.add_review(ravagh, "excellent", 5)
+amirata.add_review(ravagh, "deliciousdsadasdss", 3)
+
 
 binding.pry
 0 #leave this here to ensure binding.pry isn't the last line
