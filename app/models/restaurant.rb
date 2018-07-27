a41a82af3a854b95680e0e3d08d50fa78981f3ff
scarlettperry <si.perry15@gmail.com>
2018-07-27 10:37:04 -0400

    done

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -1,8 +1,48 @@
 class Restaurant
   attr_accessor :name
 
+  @@all = []
+
   def initialize(name)
     @name = name
+    @@all << self
+  end
+
+  def self.all
+    @@all
+  end
+
+  #come back to this one
+  # def self.find_by_name(name)
+  # end
+
+  def customer
+    restaurant_review= Review.all.select do |review_resturant|
+      review_resturant.restaurant == self
+    end
+    all_customers = restaurant_review.map do |restaurant|
+      "#{restaurant.customer.first_name} #{restaurant.customer.last_name}"
+    end
+    all_customers.uniq
+  end
+
+  def reviews
+    restaurant_review = Review.all.select do |review_resturant|
+      review_resturant.restaurant == self
+    end
+    restaurant_review.map do |restaurant|
+      restaurant.content
+    end
+  end
+
+  #didn't finsh figuring out averages
+  def average_start_rating
+    restaurant_review = Review.all.select do |review_resturant|
+      review_resturant.restaurant == self
+    end
+  restaurant_rating  = restaurant_review.map do |restaurant|
+      restaurant.rating
+    end
   end
 
 end
--- app/models/review.rb
+++ app/models/review.rb
@@ -1,4 +1,18 @@
 class Review
-  
-end
+  attr_accessor :rating, :content
+  attr_reader :customer, :restaurant
 
+  @@all = []
+
+  def self.all
+    @@all
+  end
+
+  def initialize(customer, restaurant, rating, content)
+    @customer = customer
+    @restaurant = restaurant
+    @rating = rating
+    @content = content
+    @@all << self
+  end
+end
