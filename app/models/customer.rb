0e64b329f553466e6525046f7a819dfb356af851
github username <github email address>
2018-07-27 10:17:24 -0400

    Done.

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -1,12 +1,47 @@
 class Customer
   attr_accessor :first_name, :last_name
-
+  @@all = []
   def initialize(first_name, last_name)
     @first_name = first_name
     @last_name  = last_name
+    @@all << self
   end
 
   def full_name
     "#{first_name} #{last_name}"
   end
+
+  def self.find_by_name(string)
+    @@all.find do |name|
+      name.full_name == string
+    end
+  end
+
+  def self.all_names
+    @@all.map do |name|
+      name.full_name
+    end
+  end
+
+  def add_review(restaurant,rating,content)
+    Review.new(self,restaurant,rating,content)
+  end
+
+  def reviews
+    Review.all.select do |review|
+      review.customer == self
+    end
+  end
+
+
+  def num_reviews
+    reviews.length
+  end
+
+  def restaurants
+    reviews.map do |review|
+      review.restaurant
+    end.uniq
+  end
+
 end
--- tools/console.rb
+++ tools/console.rb
@@ -6,6 +6,22 @@ end
 # Insert code here to run before hitting the binding.pry
 # This is a convenient place to define variables and/or set up new object instances,
 # so they will be available to test and play around with in your console
+# -- New Customers --
+bryan = Customer.new("Bryan","Teng")
+kyle = Customer.new("Kyle","Neale")
+
+# -- New Restaurants --
+mimis = Restaurant.new("Mimis")
+gourmet = Restaurant.new("Gourmet Place")
+
+# -- New Review --
+bryan_review = Review.new(bryan,mimis,5,"good food")
+kyle_review = Review.new(kyle,gourmet,4, "good enough")
+
+# -- Testing Methods --
+bryan.add_review(gourmet,5,"i like it")
+bryan.add_review(mimis,5,"still good")
+kyle.add_review(mimis,4, "same")
 
 binding.pry
 0 #leave this here to ensure binding.pry isn't the last line
