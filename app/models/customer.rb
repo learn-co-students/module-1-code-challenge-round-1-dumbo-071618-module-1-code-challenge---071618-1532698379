2e733855d2e88648c3a26c7378a9b0546e2b9a55
me <juniorjr083@gmail.com>
2018-07-27 10:34:28 -0400

    Done

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -1,12 +1,51 @@
 class Customer
-  attr_accessor :first_name, :last_name
-
-  def initialize(first_name, last_name)
+    attr_accessor :first_name, :last_name
+    @@all = []
+  
+    def initialize(first_name, last_name)
     @first_name = first_name
     @last_name  = last_name
+    @@all << self
   end
 
   def full_name
     "#{first_name} #{last_name}"
   end
+
+  def self.all 
+    @@all 
+  end 
+
+  def self.find_by_name(name)
+    ans  = Customer.all.find do |cust|
+    #cust.first_name == name.split[0] && cust.last_name == name.split[1] 
+    cust.full_name == name
+    end 
+   ans 
+  end 
+
+
+  def self.find_all_by_first_name(name)
+     Customer.all.select do |ele|
+       ele.first_name == name
+     end 
+
+
+  end 
+
+
+  def self.all_names
+
+    Customer.all.map do |ele|
+      ele.full_name
+
+    end 
+
+  end 
+
+
+
+
+
+
 end
--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -1,8 +1,28 @@
+#### Build out the following methods on the `Restaurant` class
+
+#- `Restaurant.all`
+  #- returns an array of all restaurants
+#- `Restaurant.find_by_name(name)`
+ # - given a string of restaurant name, returns the first restaurant that matches
+
+
 class Restaurant
   attr_accessor :name
-
+  @@all = []
   def initialize(name)
-    @name = name
+     @name = name
+     @@all << self
   end
+
+
+  def self.all
+   @@all
+  end 
+
+
+ def self.find_by_name(name)
+   Restaurant.all.find {|ele| ele.name == name}
+ end 
+
 
 end
