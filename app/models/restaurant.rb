dcb637759eda861cba5e92f120f29942dc4c8ccb
Tokuhisa1 <TokuhisaWinston@Outlook.com>
2018-07-27 10:36:48 -0400

    Done

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -1,8 +1,37 @@
 class Restaurant
   attr_accessor :name
+  @@all = []
 
   def initialize(name)
     @name = name
+    @@all << self
   end
-
+  # Restaurant.all
+  def self.all
+    # returns an array of all restaurants
+    @@all
+  end
+  # Restaurant.find_by_name(name)
+  def self.find_by_name name
+    # given a string of restaurant name, returns the first restaurant that
+    # matches
+    all.find { | restaurant | restaurant.name == name }
+  end
+  # Restaurant#customers
+  def customers
+    # Returns a unique list of all customers who have reviewed a particular
+    # restaurant.
+  end
+  # Restaurant#reviews
+  def reviews
+    # returns an array of all reviews for that restaurant
+  end
+  # Restaurant#average_star_rating
+  def average_star_rating
+    # returns the average star rating for a restaurant based on its reviews
+  end
+  # Restaurant#longest_review
+  def longest_review
+    # returns the longest review content for a given restaurant
+  end
 end
--- tools/console.rb
+++ tools/console.rb
@@ -4,8 +4,47 @@ def reload
   load 'config/environment.rb'
 end
 # Insert code here to run before hitting the binding.pry
-# This is a convenient place to define variables and/or set up new object instances,
-# so they will be available to test and play around with in your console
+# This is a convenient place to define variables and/or set up new object
+# instances, so they will be available to test and play around with in your
+# console
+# Restaurant Tests
+carmens = Restaurant.new "Carmen's"
+# puts 'This should return a restaurant named "Carmen\'s":'
+# pp carmens
+four_seasons = Restaurant.new 'the Four Seasons'
+tavern = Restaurant.new 'Tavern on the Green'
+# puts 'This should return an array of restaurant instances:'
+# pp Restaurant.all
+# puts 'This should return "the Four Seasons":'
+# pp Restaurant.find_by_name 'the Four Seasons'
 
-binding.pry
-0 #leave this here to ensure binding.pry isn't the last line
+# Customer Tests
+jane = Customer.new 'Jane', 'Doe'
+# puts 'This should return a customer named "Jane Doe":'
+# pp jane
+john1 = Customer.new 'John', 'Doe'
+emeril = Customer.new 'Emeril', 'Anonymous'
+john2 = Customer.new 'John', 'Smith'
+# puts 'This should return an array of customer instances:'
+# pp Customer.all
+# puts 'This should return "Emeril":'
+# pp Customer.find_by_name 'Emeril Anonymous'
+# puts 'This should return an array of customer instances named "John":'
+# pp Customer.find_all_by_first_name 'John'
+# puts "This should return an array of customer instances full names:"
+# pp Customer.all_names
+
+# Review Tests
+carmens_review = Review.new jane, carmens, 5, 'yum'
+# puts 'This should return a 5-star review of "yum":'
+# pp carmens_review
+four_seasons_review = Review.new john1, four_seasons, 3, 'meh'
+tavern_review = Review.new emeril, tavern, 1, 'terrible'
+# puts 'This should return an array of review instances:'
+# pp Review.all
+# puts "This should return a Jane Doe\'s instance:"
+# pp carmens_review.reviewed_by
+puts "This should return a Carmen's instance:"
+pp carmens_review.reviewed_by
+# binding.pry
+# 0 #leave this here to ensure binding.pry isn't the last line
